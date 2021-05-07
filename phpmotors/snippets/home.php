4afb55e96e54da3551733afddeb3dc191c183a82
Aaron Jones <jon17049@byui.edu>
2021-05-06 22:13:16 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/snippets/home.php
+++ phpmotors/snippets/home.php
@@ -1,74 +1,81 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en-us">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | PHP Motors</title>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="PHP Motors home page">
     <meta name="author" content="Aaron Jones">
-    <title>Home | PHP Motors</title>
-    <link href="../css/home.css" rel="stylesheet">
-    <!--<link href="https://fonts.googleapis.com/css2?family=Quantico&display=swap" rel="stylesheet">-->
+    <link rel="stylesheet" type="text/css" href="../css/home.css" media="screen">
 </head>
+
 <body>
-<header>
-    <img src="../images/site/logo.png" alt="PHP Motors Logo">
-    <p><a href="#" title="Link to your account with PHP Motors">My Account</a></p>
-    <nav>
-    <ul>
-        <li><a href="#" title="Link to the PHP Motors Home Page">Home</a></li>
-        <li><a href="#" title="Link to the PHP Motors Classic Cars">Classic</a></li>
-        <li><a href="#" title="Link to the PHP Motors Sports Cars">Sports</a></li>
-        <li><a href="#" title="Link to the PHP Motors SUVs">SUV</a></li>
-        <li><a href="#" title="Link to the PHP Motors Trucks">Trucks</a></li>
-        <li><a href="#" title="Link to the PHP Motors Used Cars">Used</a></li>
-    </ul>    
-    </nav>
-</header>
-<main>
-    <h1 class="title">Welcome to PHP Motors!</h1>
-    <div class="topdiv">
-        <h2>DMC Delorean</h2>
-        <p>3 Cup holders</p>
-        <p>Superman doors</p>
-        <p>Fuzzy dice!</p>
-        <button class="button1">Own Today</button>
+    <div class="tile">
+        <header>
+            <img src="../images/site/logo.png" alt="PHP Motors Logo">
+            <p><a href="#" title="Link to your account with PHP Motors">My Account</a></p>
+            <nav>
+                <ul>
+                    <li><a href="#" title="Link to the PHP Motors Home Page">Home</a></li>
+                    <li><a href="#" title="Link to the PHP Motors Classic Cars">Classic</a></li>
+                    <li><a href="#" title="Link to the PHP Motors Sports Cars">Sports</a></li>
+                    <li><a href="#" title="Link to the PHP Motors SUVs">SUV</a></li>
+                    <li><a href="#" title="Link to the PHP Motors Trucks">Trucks</a></li>
+                    <li><a href="#" title="Link to the PHP Motors Used Cars">Used</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <h1 class="title">Welcome to PHP Motors!</h1>
+            <div class="topdiv">
+                <h2>DMC Delorean</h2>
+                <p>3 Cup holders</p>
+                <p>Superman doors</p>
+                <p>Fuzzy dice!</p>
+                <button class="button1">Own Today</button>
+            </div>
+            <img class="delorean" src="../images/delorean.jpg" alt="image of a delorean car">
+            <button class="button2">Own Today</button>
+            <div id="split">
+                <section id="reviews">
+                    <h2>DMC Delorean Reviews</h2>
+                    <ul>
+                        <li>"So fast its almost like traveling in time." (4/5)</li>
+                        <li>"Coolest ride on the road." (4/5)</li>
+                        <li>"I'm feeling like Marty McFly!" (5/5)</li>
+                        <li>"The most futuristic ride of our day." (4.5/5)</li>
+                        <li>"80's livin and I love it!" (5/5)</li>
+                    </ul>
+                </section>
+
+                <section id="upgrades">
+                    <h2>Delorean Upgrades</h2>
+                    <ul>
+                        <li><a href="#" title="Link to Flux Capacitor Page">Flux Capacitor<img id="fluxCap"
+                                    alt=" Image of Flux Capacitor" src="../images/upgrades/flux-cap.png"></a>
+                        </li>
+                        <li><a href="#" title="Link to Flame Decals Page">Flame Decals<img id="flames"
+                                    alt=" Image of Flames" src="../images/upgrades/flame.jpg"></a>
+                        </li>
+                        <li><a href="#" title="Link to Bumper Stickers Page">Bumper Stickers<img id="sticker"
+                                    alt=" Image of Bumper Sticker" src="../images/upgrades/bumper_sticker.jpg"></a>
+                        </li>
+                        <li><a href="#" title="Link to Hub Caps Page">Hub Caps<img id="hubCap" alt=" Image of Hub Cap"
+                                    src="../images/upgrades/hub-cap.jpg"></a>
+                        </li>
+                    </ul>
+                </section>
+            </div>
+        </main>
+        <footer>
+            &copy; PHP Motors, All rights reserved.
+            <p>All images used are believed to be in "Fair Use". Please
+                notify the author if any are not and they will be
+                removed.</P>
+        </footer>
     </div>
-    <img class="delorean" src="../images/delorean.jpg" alt="image of a delorean car">
-    <button class="button2">Own Today</button>
-    <div class="reviews">
-    <h2>DMC Delorean Reviews</h2>
-    <ul>
-        <li>"So fast its almost like traveling in time." (4/5)</li>
-        <li>"Coolest ride on the road." (4/5)</li>
-        <li>"I'm feeling like Marty McFly!" (5/5)</li>
-        <li>"The most futuristic ride of our day." (4.5/5)</li>
-        <li>"80's livin and I love it!" (5/5)</li>
-    </ul>
-    </div>
-    <h2>Delorean Upgrades</h2>
-    <div id="upgrades">
-        <div class="col">
-            <img src="../images/upgrades/flux-cap.png" alt=" Image of Flux Capacitor">
-            <p><a href="#" title="Link to Flux Capacitor Page">Flux Capacitor</a></p>
-        </div>
-        <div class="col">
-            <img src="../images/upgrades/flame.jpg" alt=" Image of Flames">
-            <p><a href="#" title="Link to Flame Decals Page">Flame Decals</a></p>
-        </div>
-        <div class="col">
-            <img src="../images/upgrades/bumper_sticker.jpg" alt=" Image of Bumper Sticker">
-            <p><a href="#" title="Link to Bumper Stickers Page">Bumper Stickers</a></p>
-        </div>
-        <div class="col">
-            <img src="../images/upgrades/hub-cap.jpg" alt=" Image of Hub Cap">
-            <p><a href="#" title="Link to Hub Caps Page">Hub Caps</a></p>
-        </div>
-    </div>
-</main>
-<footer>  
-<!--<?php require $_SERVER['DOCUMENT_ROOT'].'D:\Xampp\htdocs\phpmotors\snippets\footer.php'; ?>-->  
-</footer>
 </body>
 
 </html>
