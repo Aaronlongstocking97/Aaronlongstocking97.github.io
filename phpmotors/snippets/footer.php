a82472679924ce804ee75770eaa57a4e4e4b6867
Aaron Jones <jon17049@byui.edu>
2021-05-07 12:29:12 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/snippets/footer.php
+++ phpmotors/snippets/footer.php
@@ -1,6 +1,4 @@
-<footer>
     <p>&copy; PHP Motors, All rights reserved.</p>
     <p>All images used are believed to be in "Fair Use". Please
         notify the author if any are not and they will be
         removed.</P>
-</footer>
--- phpmotors/snippets/header.php
+++ phpmotors/snippets/header.php
@@ -1,6 +1,4 @@
-<header>
     <div id="top-header">
         <img src="../images/site/logo.png" alt="PHP Motors Logo" id="logo">
         <a href="#" title="Login or Register with PHP Motors" id="acc">My Account</a>
     </div>
-</header>
