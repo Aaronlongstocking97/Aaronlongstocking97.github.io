017f781d259e62ab16aef8140a30de277c3da4b0
Aaron Jones <jon17049@byui.edu>
2021-05-10 20:18:19 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/snippets/template.php
+++ phpmotors/snippets/template.php
@@ -4,39 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <title id="page-title">Content Title | PHP Motors</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="PHP Motors template page">
-    <meta name="viewport" content="width=device-width">
     <meta name="author" content="Aaron Jones">
     <link rel="stylesheet" type="text/css" href="../css/screen.css" media="screen">
 </head>
 
 <body>
-    <div class="tile">
-        <header id="page-header">
-            <div id="logo"><img src="../images/site/logo.png" alt="PHP Motors logo"></div>
-            <div id="user">My Account</div>
+    <div id="wrapper">
+        <header>
+            <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/header.php'; ?>
         </header>
-        <nav id="page-nav">
-            <ul id="pages">
-                <li><a href="../htdocs/phpmotors/snippets/home.php">Home</a></li>
-                <li><a>Classic</a></li>
-                <li><a>Sports</a></li>
-                <li><a>SUV</a></li>
-                <li><a>Trucks</a></li>
-                <li><a>Used</a></li>
-            </ul>
+        <nav id="page_nav">
+            <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/navigation.php'; ?>
         </nav>
         <main id="main-content">
             <h1 id="content">Content Title Here</h1>
             <hr id="line-break">
-
         </main>
         <footer>
-            <p>&copy; PHP Motors, All rights reserved.</p>
-            <p>All images used are believed to be in "Fair Use". Please
-                notify the author if any are not and they will be
-                removed.</P>
-            <p>Last Updated: 3 May, 2020</p>
+            <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/footer.php'; ?>
         </footer>
     </div>
 </body>
