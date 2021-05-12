18d5c3bc83db05023a8e27fc6b2054200828f5f0
Aaron Jones <jon17049@byui.edu>
2021-05-12 11:09:04 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/library/connections.php
+++ phpmotors/library/connections.php
@@ -15,13 +15,13 @@ function phpmotorsConnect()
     // Create the actual connection object and assign it to a variable
     try {
         $link = new PDO($dsn, $username, $password, $options);
-        if (is_object($link)) {
-            echo 'It worked!';
-        }
-        //  return $link;
+        //if (is_object($link)) {
+        //    echo 'It worked!';
+        //}
+        return $link;
     } catch (PDOException $e) {
-        //  header('Location: /phpmotors/view/500.php');
-        echo "It didn't work, error: " . $e->getMessage();
+        //  echo "It didn't work, error: " . $e->getMessage();
+        header('Location: /phpmotors/view/500.php');
         exit;
     }
 }
