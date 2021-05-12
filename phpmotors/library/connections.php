0c46511005107dade2f74ff1d13f562d4706ad33
Aaron Jones <jon17049@byui.edu>
2021-05-12 11:06:18 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/library/connections.php
+++ phpmotors/library/connections.php
@@ -6,7 +6,7 @@
 function phpmotorsConnect()
 {
     $server = 'localhost';
-    $dbname = 'phpmotor';
+    $dbname = 'phpmotors';
     $username = 'iClient';
     $password = '2U2vKiB9t93tFj4O';
     $dsn = "mysql:host=$server;dbname=$dbname";
