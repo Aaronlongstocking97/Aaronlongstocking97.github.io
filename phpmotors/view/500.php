1985587216d812b654aa55c89489fdba1476e71b
Aaron Jones <jon17049@byui.edu>
2021-05-15 22:59:30 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/view/500.php
+++ phpmotors/view/500.php
@@ -1,2 +1,32 @@
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/template.php';
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <meta charset="UTF-8">
+    <title id="page-title">Content Title | PHP Motors</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="PHP Motors template page">
+    <meta name="author" content="Aaron Jones">
+    <link rel="stylesheet" type="text/css" href="../css/screen.css" media="screen">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/header.php'; ?>
+        </header>
+        <nav id="page_nav">
+            <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/navigation.php'; ?>
+        </nav>
+        <main id="main-content">
+            <h1 id="content">Content Title Here</h1>
+            <hr id="line-break">
+        </main>
+        <footer>
+            <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/footer.php'; ?>
+        </footer>
+    </div>
+</body>
+
+</html>
