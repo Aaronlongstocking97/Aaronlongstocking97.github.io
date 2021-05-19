50f3f21f54128e55cf1fbd2e81ec3beadc888d80
Aaron Jones <jon17049@byui.edu>
2021-05-19 09:50:46 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/index.php
+++ phpmotors/index.php
@@ -1,6 +1,21 @@
 <?php
 //This is the main controller
 
+// Get the array of classifications from DB using model
+$classifications = getClassifications();
+
+// Build a navigation bar using the $classifications array
+$navList = '<ul>';
+$navList .= "<li><a href='/phpmotors/index.php' title='View 
+the PHP Motors home page'>Home</a></li>";
+foreach ($classifications as $classification) {
+    $navList .= "<li><a href='/phpmotors/index.php?
+    action=" . urlencode($classification['classificationName'])
+        . "' title='View our $classification[classificationName] 
+    product line'>$classification[classificationName]</a></li>";
+}
+$navList .= '</ul>';
+
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
     $action = filter_input(INPUT_GET, 'action');
