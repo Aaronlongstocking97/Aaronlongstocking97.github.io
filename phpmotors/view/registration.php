52ded0c90bca2cd09128cf680aeccce6a56af3d6
Aaron Jones <jon17049@byui.edu>
2021-05-20 11:55:59 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/view/registration.php
+++ phpmotors/view/registration.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="PHP Motors template page">
     <meta name="author" content="Aaron Jones">
-    <link rel="stylesheet" type="text/css" href="../css/screen.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="/phpmotors/css/screen.css" media="screen">
 </head>
 
 <body>
