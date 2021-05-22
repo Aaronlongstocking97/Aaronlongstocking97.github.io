82589f68263219fd8d716d1e7ddf5fa5daf0540f
Aaron Jones <jon17049@byui.edu>
2021-05-21 21:40:53 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/view/login.php
+++ phpmotors/view/login.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title id="page-title">Template Page | PHP Motors</title>
+    <title id="page-title">Account Login | PHP Motors</title>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="PHP Motors template page">
