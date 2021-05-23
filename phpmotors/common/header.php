51214e5cc876b36428e9bd9311b769a96b707d0d
Aaron Jones <jon17049@byui.edu>
2021-05-22 21:12:37 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/common/header.php
+++ phpmotors/common/header.php
@@ -1,4 +1,4 @@
     <div id="top-header">
-        <img src="phpmotors/images/site/logo.png" alt="PHP Motors Logo" id="logo">
+        <img src="../images/site/logo.png" alt="PHP Motors Logo" id="logo">
         <a href="/phpmotors/accounts/index.php" title="Login or Register with PHP Motors" id="acc">My Account</a>
     </div>
