c3c0fa3fce346013a3e2d355bf940e85c8113bc4
Aaron Jones <jon17049@byui.edu>
2021-05-22 19:55:46 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/model/main-model.php
+++ phpmotors/model/main-model.php
@@ -25,4 +25,4 @@ function getClassifications()
 // Get the database connection file
 require_once '../library/connections.php';
 // Get the PHP Motors model for use as needed
-require_once 'model/main-model.php';
+require_once '../model/main-model.php';
