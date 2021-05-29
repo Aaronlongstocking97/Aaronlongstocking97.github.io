ca0882f9667ced75ff4e381cf2b2279109cc7c8a
Aaron Jones <jon17049@byui.edu>
2021-05-29 13:29:38 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/view/login.php
+++ phpmotors/view/login.php
@@ -29,7 +29,7 @@
                 <input type="submit" name="submit" id="regbtn" value="Sign-in">
             </form>
             <div>
-                <a href="/phpmotors/accounts/index.php?action=rgstr" id="rgstr">Not a member yet?</a>
+                <a href="/phpmotors/accounts/index.php?action=register" id="register">Not a member yet?</a>
             </div>
             <hr id="line-break">
         </main>
