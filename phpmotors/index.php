1bf9b317f2ba04db30d7fa9c65fae1959f2ba6d2
Aaron Jones <jon17049@byui.edu>
2021-05-29 20:33:00 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/index.php
+++ phpmotors/index.php
@@ -13,7 +13,7 @@ $navList = '<ul>';
 $navList .= "<li><a href='/phpmotors/index.php' title='View 
 the PHP Motors home page'>Home</a></li>";
 foreach ($classifications as $classification) {
-    $navList .= "<li><a href='/phpmotors/index.php?
+    $navList .= "<li><a href='/phpmotors/index.php
     action=" . urlencode($classification['classificationName'])
         . "' title='View our $classification[classificationName] 
     product line'>$classification[classificationName]</a></li>";
