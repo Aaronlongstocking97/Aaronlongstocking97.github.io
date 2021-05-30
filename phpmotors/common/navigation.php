51aafc28ecc2b1b11f5490bece289b95602a0ed0
Aaron Jones <jon17049@byui.edu>
2021-05-29 19:53:41 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/common/navigation.php
+++
@@ -1,8 +0,0 @@
-    <ul id="pages">
-        <li><a href="/phpmotors/" title="PHP Motors home page">Home</a></li>
-        <li><a href="#" title="classic cars page">Classic</a></li>
-        <li><a href="#" title="sports cars">Sports</a></li>
-        <li><a href="#" title="SUV">SUV</a></li>
-        <li><a href="#" title="Trucks">Trucks</a></li>
-        <li><a href="#" title="used cars">Used</a></li>
-    </ul>
