fa1884b6c15f43a69e103df82f5f83d5f62b91cc
Aaron Jones <jon17049@byui.edu>
2021-06-01 18:35:39 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/accounts/index.php
+++ phpmotors/accounts/index.php
@@ -6,7 +6,7 @@
 // Get the database connection file
 require_once '../library/connections.php';
 // Get the phpmotors model for use as needed
-require_once '../model/phpmotors-model.php';
+require_once '../model/main-model.php';
 // Get the accounts model
 require_once '../model/accounts-model.php';
 
