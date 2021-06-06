61f9ddd473c0d9a5ce35c532f29f37472cf697ce
Aaron Jones <jon17049@byui.edu>
2021-06-05 22:23:24 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

---
+++ phpmotors/view/add-classification.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <meta charset="UTF-8">
+    <title id="page-title">Add Classsification | PHP Motors</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="PHP Motors Vehicle Management page">
+    <meta name="author" content="Aaron Jones">
+    <link rel="stylesheet" type="text/css" href="/phpmotors/css/vehicle-man.css" media="screen">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/header.php'; ?>
+        </header>
+        <nav id="page_nav">
+            <?php echo $navList; ?>
+        </nav>
+        <main id="main-content">
+            <h1 id="content">Add Car Classsification</h1>
+            <ul id="addOn">
+                <li><a href="/phpmotors/vehicles/index.php?action=addClass" id="addClass">Add Classsification</a></li>
+                <li><a href="/phpmotors/vehicles/index.php?action=addVehicle" id="addVehicle">Add Vehicle</a></li>
+            </ul>
+            <hr id="line-break">
+        </main>
+        <footer>
+            <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/footer.php'; ?>
+        </footer>
+    </div>
+</body>
+
+</html>
