e18ed5b324ef4500848a987c96d9333ecc200d4c
Aaron Jones <jon17049@byui.edu>
2021-06-06 00:31:08 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/view/add-vehicle.php
+++ phpmotors/view/add-vehicle.php
@@ -31,7 +31,7 @@
                 <input type="text" id="make" name="vehicleMake">
                 <label for="model">Model</label>
                 <input type="text" id="model" name="vehicleModel">
-                <textarea name="vehicleDescription" id="specify" cols="20" rows="2"></textarea>
+                <textarea name="vehicleDescription" id="specify" cols="20" rows="2">Description</textarea>
                 <label for="imgPath">Image Path</label>
                 <input type="text" id="imgPath" name="vehicleImagePath">
                 <label for="thumbPath">Thumbnail Path</label>
