6bcfade595e8b2d7fb8bda650d99827af4f8d29c
Aaron Jones <jon17049@byui.edu>
2021-06-06 20:25:06 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/view/add-vehicle.php
+++ phpmotors/view/add-vehicle.php
@@ -28,21 +28,21 @@
                     <option value="Choose Car Classification"></option>
                 </select>
                 <label for="make">Make</label>
-                <input type="text" id="make" name="vehicleMake">
+                <input type="text" id="make" name="vehicleMake" size="5">
                 <label for="model">Model</label>
-                <input type="text" id="model" name="vehicleModel">
+                <input type="text" id="model" name="vehicleModel" size="5">
                 <label for="specify">Description</label>
                 <textarea name="vehicleDescription" id="specify" cols="20" rows="2"></textarea>
                 <label for="imgPath">Image Path</label>
-                <input type="text" id="imgPath" name="vehicleImagePath">
+                <input type="text" id="imgPath" name="vehicleImagePath" size="5">
                 <label for="thumbPath">Thumbnail Path</label>
-                <input type="text" id="thumbPath" name="vehicleThumbnailPath">
+                <input type="text" id="thumbPath" name="vehicleThumbnailPath" size="5">
                 <label for="price">Price</label>
-                <input type="number" step="any" id="price" name="vehiclePrice">
+                <input type="number" step="any" id="price" name="vehiclePrice" size="5">
                 <label for="quantity"># In Stock</label>
-                <input type="number" id="quantity" name="vehiclesInStock">
+                <input type="number" id="quantity" name="vehiclesInStock" size="5">
                 <label for="color">Color</label>
-                <input type="text" id="color" name="vehicleColor">
+                <input type="text" id="color" name="vehicleColor" size="5">
                 <input type="submit" name="submit" id="addVehicleBtn" value="Add Vehicle">
                 <!-- <input type="hidden" name="action" value="register"> -->
             </form>
