e4038c08c172dced5d2378456589e267a833929b
Aaron Jones <jon17049@byui.edu>
2021-06-07 22:09:03 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/vehicles/index.php
+++ phpmotors/vehicles/index.php
@@ -28,7 +28,9 @@ $navList .= '</ul>';
 $carClassifications = '<select>';
 $carClassifications .= '<option>Choose Car Classification</option>';
 foreach ($classifications as $classification) {
-    $carClassifications .= "<option value=" . $classification['classificationId'] . ">" . $classification['classificationName'] . "</option>";
+    $carClassifications .= "<option value="
+        . $classification['classificationId'] . ">"
+        . $classification['classificationName'] . "</option>";
 }
 $carClassifications .= '</select>';
 
