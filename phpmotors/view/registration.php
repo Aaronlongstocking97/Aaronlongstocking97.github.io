1990c5bf82f7ac9a5431b10eed5e7bc4840399b5
Aaron Jones <jon17049@byui.edu>
2021-06-09 23:13:49 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/view/registration.php
+++ phpmotors/view/registration.php
@@ -27,16 +27,35 @@
             }
             ?>
             <form action="/phpmotors/accounts/index.php" id="align" method="post">
-                <label for="clientFirstname">First Name</label>
-                <input type="text" id="clientFirstname" name="clientFirstname">
-                <label for="clientLastname">Last Name</label>
-                <input type="text" id="clientLastname" name="clientLastname">
-                <label for="clientEmail">Email</label>
-                <input type="email" id="clientEmail" name="clientEmail">
-                <label for="clientPassword">Password</label>
-                <input type="password" id="clientPassword" name="clientPassword">
-                <input type="submit" name="submit" id="regbtn" value="Register">
-                <input type="hidden" name="action" value="register">
+                <fieldset>
+                    <label for="clientFirstname">First Name</label>
+                    <input type="text" id="clientFirstname" name="clientFirstname" <?php if (isset($clientFirstname)) {
+                                                                                        echo "value='$clientFirstname'";
+                                                                                    }
+                                                                                    ?> required>
+                    <label for="clientLastname">Last Name</label>
+                    <input type="text" id="clientLastname" name="clientLastname" <?php if (isset($clientLastname)) {
+                                                                                        echo "value='$clientLastname'";
+                                                                                    }
+                                                                                    ?> required>
+                    <label for="clientEmail">Email</label>
+                    <input type="email" id="clientEmail" name="clientEmail" required placeholder="Enter a valid email address" <?php if (isset($clientEmail)) {
+                                                                                                                                    echo "value='$clientEmail'";
+                                                                                                                                }
+                                                                                                                                ?> required>
+                    <label for="clientPassword">Password</label>
+                    <span>Passwords must be at least 8 characters and contain at least 1
+                        number, 1 capital letter and 1 special character</span>
+                    <input type="password" id="clientPassword" name="clientPassword" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])
+                    (?=.*[A-Z])(?=.*[a-z]).*$">
+                    <?php if (isset($clientPassword)) {
+                        echo "value='$clientPassword'";
+                    }
+                    ?> required>
+                    <label>&nbsp;</label>
+                    <input type="submit" name="submit" id="regbtn" value="Register">
+                    <input type="hidden" name="action" value="register">
+                </fieldset>
             </form>
             <hr id="line-break">
         </main>
