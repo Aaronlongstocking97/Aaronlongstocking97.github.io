5dc6cea1f4a7bc38b8b00040b1608c2bcc9b59df
Aaron Jones <jon17049@byui.edu>
2021-06-09 23:42:34 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/library/functions.php
+++ phpmotors/library/functions.php
@@ -1,1 +1,10 @@
 <?php
+/*
+* Usable funtions for multiple files
+*/
+
+function checkEmail($clientEmail)
+{
+    $valEmail = filter_var($clientEmail, FILTER_VALIDATE_EMAIL);
+    return $valEmail;
+}
