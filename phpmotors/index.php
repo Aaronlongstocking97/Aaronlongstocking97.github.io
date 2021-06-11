eed59153946dc1a557e9f0ec359baf4b17f6f677
Aaron Jones <jon17049@byui.edu>
2021-06-10 22:40:39 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/index.php
+++ phpmotors/index.php
@@ -5,7 +5,7 @@ require_once './library/connections.php';
 // Get the main model for use as needed
 require_once './model/main-model.php';
 // Get the functions library
-require_once '../library/functions.php';
+require_once './library/functions.php';
 
 // Get the array of classifications from DB using model
 $classifications = getClassifications();
