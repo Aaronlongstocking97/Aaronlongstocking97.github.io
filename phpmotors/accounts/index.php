76006d8b4f910c839ea8a6204f95806c8e432fe7
Aaron Jones <jon17049@byui.edu>
2021-06-10 22:39:45 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/accounts/index.php
+++ phpmotors/accounts/index.php
@@ -14,18 +14,8 @@ require_once '../library/functions.php';
 
 // Get the array of classifications
 $classifications = getClassifications();
-
 // Build a navigation bar using the $classifications array
-$navList = '<ul>';
-$navList .= "<li><a href='/phpmotors/index.php' title='View 
-the PHP Motors home page'>Home</a></li>";
-foreach ($classifications as $classification) {
-    $navList .= "<li><a href='/phpmotors/index.php?action="
-        . urlencode($classification['classificationName'])
-        . "' title='View our $classification[classificationName] 
-    product line'>$classification[classificationName]</a></li>";
-}
-$navList .= '</ul>';
+$navList = buildNavigation($classifications);
 
 // Get the value from the action name - value pair
 $action = filter_input(INPUT_POST, 'action');
--- phpmotors/index.php
+++ phpmotors/index.php
@@ -7,18 +7,8 @@ require_once './model/main-model.php';
 
 // Get the array of classifications from DB using model
 $classifications = getClassifications();
-
 // Build a navigation bar using the $classifications array
-$navList = '<ul>';
-$navList .= "<li><a href='/phpmotors/index.php' title='View 
-the PHP Motors home page'>Home</a></li>";
-foreach ($classifications as $classification) {
-    $navList .= "<li><a href='/phpmotors/index.php?action="
-        . urlencode($classification['classificationName'])
-        . "' title='View our $classification[classificationName] 
-    product line'>$classification[classificationName]</a></li>";
-}
-$navList .= '</ul>';
+$navList = buildNavigation($classifications);
 
 $action = filter_input(INPUT_GET, 'action');
 if ($action == NULL) {
