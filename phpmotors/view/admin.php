f32c736d2279eac61d6cc6aa2816c957558e4081
Aaron Jones <jon17049@byui.edu>
2021-06-12 16:50:24 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

---
+++ phpmotors/view/admin.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <meta charset="UTF-8">
+    <title id="page-title">Admin Page | PHP Motors</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="PHP Motors admin page">
+    <meta name="author" content="Aaron Jones">
+    <link rel="stylesheet" type="text/css" href="/phpmotors/css/login.css" media="screen">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/header.php'; ?>
+        </header>
+        <nav id="page_nav">
+            <?php echo $navList; ?>
+        </nav>
+        <main id="main-content">
+            <h1 id="content">Sign in</h1>
+            <?php
+            if (isset($_SESSION['message'])) {
+                echo $_SESSION['message'];
+            }
+            ?>
+            <form action="/phpmotors/accounts/" method="post">
+                <fieldset id="align">
+                    <label for="clientEmail">Email</label>
+                    <input type="email" id="clientEmail" name="clientEmail" required<?php if (isset($clientEmail)) {
+                                                                                        echo "value='$clientEmail'";
+                                                                                    }
+                                                                                    ?>>
+                    <label for="clientPassword">Password</label>
+                    <input type="password" id="clientPassword" name="clientPassword" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$">
+                    <label>&nbsp;</label>
+                    <input type="submit" name="submit" id="regbtn" value="Sign-in">
+                    <input type="hidden" name="action" value="Sign-in">
+                    <a href="/phpmotors/accounts/?action=registration" id="
+                    registration">Not a member yet?</a>
+                </fieldset>
+            </form>
+            <hr id="line-break">
+        </main>
+        <footer>
+            <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/footer.php'; ?>
+        </footer>
+    </div>
+</body>
+
+</html>
