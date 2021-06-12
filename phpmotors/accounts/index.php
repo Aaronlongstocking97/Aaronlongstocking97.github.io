b7cc6e1dc457b2c7e8fcf4c9c584a301b7cbe573
Aaron Jones <jon17049@byui.edu>
2021-06-12 15:36:02 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/accounts/index.php
+++ phpmotors/accounts/index.php
@@ -105,26 +105,42 @@ switch ($action) {
 
     case 'Sign-in':
         // Filter and store the data
-        $clientEmail = filter_input(
-            INPUT_POST,
-            'clientEmail',
-            FILTER_SANITIZE_EMAIL
-        );
-        $clientPassword = filter_input(
-            INPUT_POST,
-            'clientPassword',
-            FILTER_SANITIZE_STRING
-        );
-
+        $clientEmail = filter_input(INPUT_POST, 'clientEmail', FILTER_SANITIZE_EMAIL);
         $clientEmail = checkEmail($clientEmail);
+        $clientPassword = filter_input(INPUT_POST, 'clientPassword', FILTER_SANITIZE_STRING);
+        $passwordCheck = checkPassword($clientPassword);
 
-        // Check for missing data
-        if (empty($clientEmail) || empty($clientPassword)) {
-            $message = '<p>Please provide information for all 
-            empty form fields.</p>';
+        // Run basic checks, return if errors
+        if (empty($clientEmail) || empty($passwordCheck)) {
+            $message = '<p class="notice">Please provide a valid email address and password.</p>';
             include '../view/login.php';
             exit;
         }
+
+        // A valid password exists, proceed with the login process
+        // Query the client data based on the email address
+        $clientData = getClient($clientEmail);
+        // Compare the password just submitted against
+        // the hashed password for the matching client
+        $hashCheck = password_verify($clientPassword, $clientData['clientPassword']);
+        // If the hashes don't match create an error
+        // and return to the login view
+        if (!$hashCheck) {
+            $message = '<p class="notice">Please check your password and try again.</p>';
+            include '../view/login.php';
+            exit;
+        }
+        // A valid user exists, log them in
+        $_SESSION['loggedin'] = TRUE;
+        // Remove the password from the array
+        // the array_pop function removes the last
+        // element from an array
+        array_pop($clientData);
+        // Store the array into the session
+        $_SESSION['clientData'] = $clientData;
+        // Send them to the admin view
+        include '../view/admin.php';
+        exit;
         break;
 
     case 'login':
