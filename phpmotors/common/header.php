d5fd25ee913cb85a6daeb74ee1ddf3fedebe2521
Aaron Jones <jon17049@byui.edu>
2021-06-12 18:39:13 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/common/header.php
+++ phpmotors/common/header.php
@@ -1,7 +1,7 @@
     <div id="top-header">
         <img src="/phpmotors/images/site/logo.png" alt="PHP Motors Logo" id="logo">
-        <?php if (isset($_SESSION['sessionFirstname'])) {
-            echo $_SESSION['sessionFirstname'];
+        <?php if (isset($_SESSION['user'])) {
+            echo $_SESSION['user'];
         }
         ?>
         <a href="/phpmotors/accounts/index.php?action=login" title="Login or 
