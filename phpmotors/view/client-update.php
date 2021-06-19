699cc9ea4a86e7d4b809127adb5dd9dacc10a0c1
Aaron Jones <jon17049@byui.edu>
2021-06-19 17:31:48 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/view/client-update.php
+++ phpmotors/view/client-update.php
@@ -27,7 +27,38 @@ if (!isset($_SESSION['loggedin'])) {
             <?php echo $navList; ?>
         </nav>
         <main id="main-content">
-            <h1>Client Update</h1>
+            <form action="/phpmotors/accounts/index.php" method="post">
+                <fieldset id="align">
+                    <label for="clientFirstname">First Name</label>
+                    <input type="text" id="clientFirstname" name="clientFirstname" <?php if (isset($clientFirstname)) {
+                                                                                        echo "value='$clientFirstname'";
+                                                                                    }
+                                                                                    ?> required>
+                    <label for="clientLastname">Last Name</label>
+                    <input type="text" id="clientLastname" name="clientLastname" <?php if (isset($clientLastname)) {
+                                                                                        echo "value='$clientLastname'";
+                                                                                    }
+                                                                                    ?> required>
+                    <label for="clientEmail">Email</label>
+                    <input type="email" id="clientEmail" name="clientEmail" required placeholder="Enter a valid email address" <?php if (isset($clientEmail)) {
+                                                                                                                                    echo "value='$clientEmail'";
+                                                                                                                                }
+                                                                                                                                ?> required>
+
+                    <!-- used for white space between lines -->
+                    <label>&nbsp;</label>
+                    <input type="submit" name="submit" id="regbtn" value="Register">
+                    <input type="hidden" name="action" value="register">
+                </fieldset>
+            </form>
+            <form action="/phpmotors/accounts/index.php" method="post">
+                <fieldset id="align">
+                    <label for="clientPassword">Password</label>
+                    <span>Passwords must be at least 8 characters and contain at least 1
+                        number, 1 capital letter and 1 special character</span>
+                    <input type="password" id="clientPassword" name="clientPassword" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$">
+                </fieldset>
+            </form>
             <hr id="line-break">
         </main>
         <footer>
