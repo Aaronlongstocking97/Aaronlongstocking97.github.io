c582911b1eae462662a6fc9241d8a941201cd5ef
Aaron Jones <jon17049@byui.edu>
2021-06-18 23:15:26 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/common/header.php
+++ phpmotors/common/header.php
@@ -5,7 +5,7 @@
             $clientFirstName = $_SESSION['clientData']['clientFirstname'];
             echo '<a href="/phpmotors/accounts/index.php?action=User" title="PHP Motors Admin View" id="User">'
                 . $clientFirstName
-                . '</a>'
+                . '   |   </a>'
                 . '<a href="/phpmotors/accounts/index.php?action=Logout" title="Logout of PHP Motors" id="logout">Logout</a>';
         } else {
             echo '<a href="/phpmotors/accounts/index.php?action=login" title="Login or Register with PHP Motors" id="login">My Account</a>';
