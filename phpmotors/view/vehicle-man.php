ba517f138ed8381b6beffba395eb04eb9a042862
Aaron Jones <jon17049@byui.edu>
2021-06-19 16:23:23 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/view/vehicle-man.php
+++ phpmotors/view/vehicle-man.php
@@ -3,6 +3,9 @@ if ($_SESSION['clientData']['clientLevel'] < 2) {
     header('location: /phpmotors/');
     exit;
 }
+if (isset($_SESSION['message'])) {
+    $message = $_SESSION['message'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="en-us">
@@ -56,3 +59,4 @@ if ($_SESSION['clientData']['clientLevel'] < 2) {
 </body>
 
 </html>
+<?php unset($_SESSION['message']); ?>
