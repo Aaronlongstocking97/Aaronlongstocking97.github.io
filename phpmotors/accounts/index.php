91e45dc4541fdcf4d41e213fe12b867356753e36
Aaron Jones <jon17049@byui.edu>
2021-06-20 00:30:30 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/accounts/index.php
+++ phpmotors/accounts/index.php
@@ -166,8 +166,6 @@ switch ($action) {
         $clientEmail = filter_input(INPUT_POST, 'clientEmail', FILTER_SANITIZE_EMAIL);
         $clientId = filter_input(INPUT_POST, 'clientId', FILTER_SANITIZE_NUMBER_INT);
 
-        $clientEmail = checkEmail($clientEmail);
-
         if (
             empty($clientFirstname) || empty($clientLastname) || empty($clientEmail)
             || empty($clientId)
@@ -177,16 +175,16 @@ switch ($action) {
             exit;
         }
 
-        // // Check for existing email
-        // $existingEmail = checkExistingEmail($clientEmail);
+        // Check for existing email
+        $existingEmail = checkExistingEmail($clientEmail);
 
-        // // deal with existing email during registration
-        // if ($existingEmail) {
-        //     $message = '<p class="notice">That email address 
-        //         already exists. Do you want to login instead?</p>';
-        //     include '../view/client-update.php';
-        //     exit;
-        // }
+        // deal with existing email during registration
+        if ($existingEmail === 1) {
+            $message = '<p class="notice">That email address 
+                already exists. Do you want to login instead?</p>';
+            include '../view/client-update.php';
+            exit;
+        }
 
         $updateClientResult = updateClient($clientFirstname, $clientLastname, $clientEmail, $clientId);
         $clientData = getClientInfo($clientId);
