bab8a212bcd3a5e69c7b9667dcdeb04a2ca1be2a
Aaron Jones <jon17049@byui.edu>
2021-06-19 21:30:44 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/view/client-update.php
+++ phpmotors/view/client-update.php
@@ -32,33 +32,47 @@ if (!isset($_SESSION['loggedin'])) {
             <form action="/phpmotors/accounts/index.php" method="post">
                 <fieldset id="align">
                     <label for="clientFirstname">First Name</label>
-                    <input type="text" id="clientFirstname" name="clientFirstname" <?php if (isset($clientFirstname)) {
-                                                                                        echo "value='$clientFirstname'";
-                                                                                    }
-                                                                                    ?> required>
-                    <label for="clientLastname">Last Name</label>
-                    <input type="text" id="clientLastname" name="clientLastname" <?php if (isset($clientLastname)) {
-                                                                                        echo "value='$clientLastname'";
-                                                                                    }
-                                                                                    ?> required>
-                    <label for="clientEmail">Email</label>
-                    <input type="email" id="clientEmail" name="clientEmail" required placeholder="Enter a valid email address" <?php if (isset($clientEmail)) {
-                                                                                                                                    echo "value='$clientEmail'";
-                                                                                                                                }
-                                                                                                                                ?> required>
-
-                    <!-- used for white space between lines -->
                     <label>&nbsp;</label>
-                    <input type="submit" name="submit" id="regbtn" value="Register">
-                    <input type="hidden" name="action" value="register">
+                    <input type="text" id="clientFirstname" name="clientFirstname" required <?php if (isset($clientFirstname)) {
+                                                                                                echo "value='$clientFirstname'";
+                                                                                            } elseif (isset($invInfo['clientFirstname'])) {
+                                                                                                echo "value='$invInfo[clientFirstname]'";
+                                                                                            } ?>>
+                    <label for="clientLastname">Last Name</label>
+                    <label>&nbsp;</label>
+                    <input type="text" id="clientLastname" name="clientLastname" required <?php if (isset($clientLastname)) {
+                                                                                                echo "value='$clientLastname'";
+                                                                                            } elseif (isset($invInfo['clientLastname'])) {
+                                                                                                echo "value='$invInfo[clientLastname]'";
+                                                                                            } ?>>
+                    <label for="clientEmail">Email</label>
+                    <label>&nbsp;</label>
+                    <input type="email" id="clientEmail" name="clientEmail" required <?php if (isset($clientEmail)) {
+                                                                                            echo "value='$clientEmail'";
+                                                                                        } elseif (isset($invInfo['clientEmail'])) {
+                                                                                            echo "value='$invInfo[clientEmail]'";
+                                                                                        } ?>>
+
+                    <label>&nbsp;</label>
+                    <input type="submit" name="submit" id="regbtn" value="Update Info">
+                    <input type="hidden" name="action" value="">
+                    <input type="hidden" name="clientId" value="<?php if (isset($clientInfo['clientId'])) {
+                                                                    echo $clientInfo['clientId'];
+                                                                } elseif (isset($clientId)) {
+                                                                    echo $clientId;
+                                                                } ?>">
                 </fieldset>
             </form>
             <h2>Update Password</h2>
             <form action="/phpmotors/accounts/index.php" method="post">
                 <fieldset id="align">
-                    <label for="clientPassword">Password</label>
                     <span>Passwords must be at least 8 characters and contain at least 1
                         number, 1 capital letter and 1 special character</span>
+                    <label>&nbsp;</label>
+                    <span>*note your original password will be changed.</span>
+                    <label>&nbsp;</label>
+                    <label for="clientPassword">Password</label>
+                    <label>&nbsp;</label>
                     <input type="password" id="clientPassword" name="clientPassword" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$">
                 </fieldset>
             </form>
