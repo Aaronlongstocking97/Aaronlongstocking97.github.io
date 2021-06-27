d701ee40c5b9f388d3aca3802a34eeb364f4184b
Aaron Jones <jon17049@byui.edu>
2021-06-26 23:23:46 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/view/classification.php
+++ phpmotors/view/classification.php
@@ -3,10 +3,10 @@
 
 <head>
     <meta charset="UTF-8">
-    <title id="page-title">Account Login | PHP Motors</title>
+    <title><?php echo $classificationName; ?> vehicles | PHP Motors, Inc.</title>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="PHP Motors login page">
+    <meta name="description" content="PHP Motors classification page">
     <meta name="author" content="Aaron Jones">
     <link rel="stylesheet" type="text/css" href="/phpmotors/css/login.css" media="screen">
 </head>
@@ -20,30 +20,17 @@
             <?php echo $navList; ?>
         </nav>
         <main id="main-content">
-            <h1 id="content">Sign in</h1>
+            <h1 id="content"><?php echo $classificationName; ?> vehicles</h1>
             <?php
-            if (isset($_SESSION['message'])) {
-                echo $_SESSION['message'];
-            }
             if (isset($message)) {
                 echo $message;
             }
             ?>
-            <form action="/phpmotors/accounts/" method="post">
-                <fieldset id="align">
-                    <label for="clientEmail">Email</label>
-                    <input type="email" id="clientEmail" name="clientEmail" required<?php if (isset($clientEmail)) {
-                                                                                        echo "value='$clientEmail'";
-                                                                                    }
-                                                                                    ?>>
-                    <label for="clientPassword">Password</label>
-                    <input type="password" id="clientPassword" name="clientPassword" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$">
-                    <label>&nbsp;</label>
-                    <input type="submit" name="submit" id="regbtn" value="Sign-in">
-                    <input type="hidden" name="action" value="Sign-in">
-                    <a href="/phpmotors/accounts/?action=registration" id="registration">Not a member yet?</a>
-                </fieldset>
-            </form>
+            <?php
+            if (isset($vehicleDisplay)) {
+                echo $vehicleDisplay;
+            }
+            ?>
             <hr id="line-break">
         </main>
         <footer>
