3bf9681eb437621edbc17a362af3d1cc3ec843bd
Aaron Jones <jon17049@byui.edu>
2021-07-01 14:31:12 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/view/vehicle-detail.php
+++ phpmotors/view/vehicle-detail.php
@@ -21,7 +21,7 @@
             <?php echo $navList; ?>
         </nav>
         <main>
-            <h1 id="content"><?php echo $invMake, $invModel; ?></h1>
+            <h1 id="content"><?php echo $classificationName; ?></h1>
             <?php
             if (isset($message)) {
                 echo $message;
