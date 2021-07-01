db227de14bc3b8997c43884d7ad38029418b4e83
Aaron Jones <jon17049@byui.edu>
2021-07-01 15:06:17 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/view/vehicle-detail.php
+++ phpmotors/view/vehicle-detail.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title> vehicles | PHP Motors, Inc.</title>
+    <title> <?php echo $vehiclesDetail['invMake'] . " " .  $vehiclesDetail['invModel'] ?> Details | PHP Motors, Inc.</title>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="PHP Motors vehicle details page">
