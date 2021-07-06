dad2014e0a22793002e8310bebe6c15bbe49dacf
Aaron Jones <jon17049@byui.edu>
2021-07-06 15:49:31 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

---
+++ phpmotors/view/image-admin.php
@@ -0,0 +1,70 @@
+<?php
+if (isset($_SESSION['message'])) {
+    $message = $_SESSION['message'];
+}
+?>
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <meta charset="UTF-8">
+    <title id="page-title">Admin Page | PHP Motors</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="PHP Motors admin page">
+    <meta name="author" content="Aaron Jones">
+    <link rel="stylesheet" type="text/css" href="/phpmotors/css/login.css" media="screen">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/header.php'; ?>
+        </header>
+        <nav id="page_nav">
+            <?php echo $navList; ?>
+        </nav>
+        <main id="main-content">
+            <?php
+            echo "<h1 id='displayName'>" . $_SESSION['clientData']['clientFirstname'] . "    " . $_SESSION['clientData']['clientLastname'] . "</h1>" ?>
+            <?php
+            if (isset($message)) {
+                echo $message;
+            }
+            ?>
+            <span>You are logged in.</span>
+            <?php
+            echo "<ul>"
+                . "<li>First name:  " . $_SESSION['clientData']['clientFirstname'] . "</li>"
+                . "<li>Last name:   " . $_SESSION['clientData']['clientLastname'] . "</li>"
+                . "<li>Email:   " . $_SESSION['clientData']['clientEmail'] . "</li>"
+                . "</ul>" ?>
+            <h2 id='accountManage'>Account Management</h2>
+            <span>Use this link to update account information.</span>
+            <br>
+            <div>&nbsp;</div>
+            <a href="/phpmotors/accounts/?action=client-update&clientId=  <?php echo $_SESSION['clientData']['clientId'] ?>">Update Account Information</a>
+            <div>&nbsp;</div>
+            <?php
+            $level = $_SESSION['clientData']['clientLevel'];
+            if ($level > 1) {
+                echo "<h2 id='adminInvManage'>Inventory Management</h2>"
+                    . "<span>Use this link to manage the inventory.</span>"
+                    . "<br>"
+                    . "<div>&nbsp;</div>"
+                    . '<a href="/phpmotors/vehicles/?action=vehicle-man" id="vehicle-man">Vehicle Management</a>'
+                    . "<div>&nbsp;</div>";
+            }
+            ?>
+            <hr id="line-break">
+        </main>
+        <footer>
+            <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/footer.php'; ?>
+        </footer>
+    </div>
+</body>
+
+</html>
+<?php
+unset($_SESSION['message']);
+?>
