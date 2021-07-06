4d01ae15b35d7377703c92f4f33d8f5b0f3fa0bf
Aaron Jones <jon17049@byui.edu>
2021-07-06 12:28:34 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/library/functions.php
+++ phpmotors/library/functions.php
@@ -12,8 +12,7 @@ function buildNavigation($classifications)
     $navList .= "<li><a href='/phpmotors/' title='View 
                 the PHP Motors home page'>Home</a></li>";
     foreach ($classifications as $classification) {
-        $navList .= "<li><a href='/phpmotors/vehicles?
-        action=classification&classificationName="
+        $navList .= "<li><a href='/phpmotors/vehicles?action=classification&classificationName="
             . urlencode($classification['classificationName'])
             . "' title='View our $classification[classificationName] lineup
             of vehicles'>$classification[classificationName]</a></li>";
