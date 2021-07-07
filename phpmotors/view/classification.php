941e67585d34303b3458ad46fb6859d3b430ad2d
Aaron Jones <jon17049@byui.edu>
2021-07-06 20:02:27 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/view/classification.php
+++ phpmotors/view/classification.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title><?php echo $imgPrimary; ?> vehicles | PHP Motors, Inc.</title>
+    <title><?php echo $imgPath; ?> vehicles | PHP Motors, Inc.</title>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="PHP Motors classification page">
@@ -20,7 +20,7 @@
             <?php echo $navList; ?>
         </nav>
         <main>
-            <h1 id="content"><?php echo $imgPrimary; ?> vehicles</h1>
+            <h1 id="content"><?php echo $imgPath; ?> vehicles</h1>
             <?php
             if (isset($message)) {
                 echo $message;
