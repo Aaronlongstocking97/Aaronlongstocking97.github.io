82b44f14da5f42767c6f790b86345ade79c8e098
Aaron Jones <jon17049@byui.edu>
2021-07-09 16:43:25 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- phpmotors/vehicles/index.php
+++ phpmotors/vehicles/index.php
@@ -288,7 +288,7 @@ switch ($action) {
 
         $vehicleThumbnail = getThumbnailImages($invId);
 
-        if (empty($vehicleThumbnail)) {
+        if (!count($vehicleThumbnail)) {
             $message = "<p class='notice'>Sorry, 
             no vehicle thumbnails could be found.</p>";
         }
