936a36b9a81ca63c4b3251ddf5a3ca019c0d2ee3
AaronLongstocking97 <jon17049@byui.edu>
2021-11-04 21:29:34 -0600

    Signed-off-by: AaronLongstocking97 <jon17049@byui.edu>

--- assets/CSE 121B/sandbox/js/main1.js
+++ assets/CSE 121B/sandbox/js/main1.js
@@ -1,7 +1,10 @@
+// Changed PI to pi 
 const pi = 3.14;
-const radius = 3;
+// Changed Const to let
+let radius = 3;
 let area = 0;
 area = radius * radius * pi;
 console.log(area);
 radius = 4;
 area = radius * radius * pi;
+console.log(area);
