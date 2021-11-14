235ca5054709913220d64500004ec2074abaf5c8
AaronLongstocking97 <jon17049@byui.edu>
2021-11-13 23:19:25 -0700

    Signed-off-by: AaronLongstocking97 <jon17049@byui.edu>

--- assets/CSE 121B/Week 2/js/task2.js
+++ assets/CSE 121B/Week 2/js/task2.js
@@ -3,10 +3,10 @@
 /* VARIABLES */
 let name = 'Aaron Jones';
 // Step 1: declare and instantiate a variable to hold your name
-
+let theNameElement = document.querySelector('#name');
 
 // Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
-
+document.querySelector('#name');
 
 // Step 3: declare and instantiate a variable to hold the current year
 
