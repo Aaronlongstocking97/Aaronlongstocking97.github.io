729475cfc250a01111d48d93e5f5ff7694840d4b
AaronLongstocking97 <jon17049@byui.edu>
2021-11-27 23:46:14 -0700

    Signed-off-by: AaronLongstocking97 <jon17049@byui.edu>

--- assets/CSE 121B/Week 4/js/task4.js
+++ assets/CSE 121B/Week 4/js/task4.js
@@ -14,7 +14,7 @@ myInfo = {
     photo: '/Week 4/images/photo.JPG'
 };
 // Step 4: Add another property named favoriteFoods with a value of an array of your favorite foods as strings ( hint: [] )
-myInfo = { 
+myInfo = {
     name: 'Aaron Jones',
     photo: '/Week 4/images/photo.JPG',
     favoriteFoods: ['chocolate', 'iceCream', 'brownies', 'cookies', 'sourGummyWorms']
@@ -23,7 +23,12 @@ myInfo = {
 myInfo = {
     name: 'Aaron Jones',
     photo: '/Week 4/images/photo.JPG',
-    favoriteFoods: ['chocolate', 'iceCream', 'brownies', 'cookies', 'sourGummyWorms']
+    favoriteFoods: [
+        'chocolate',
+        'iceCream',
+        'brownies',
+        'cookies', 'sourGummyWorms'
+    ]
 };
 // Step 6: Add another property named placesLived with a value of an empty array
 
