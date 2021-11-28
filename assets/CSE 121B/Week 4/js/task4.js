c51440320214cf7e9e882d34521fea55c22ed80e
AaronLongstocking97 <jon17049@byui.edu>
2021-11-27 22:52:27 -0700

    Signed-off-by: AaronLongstocking97 <jon17049@byui.edu>

--- assets/CSE 121B/Week 4/js/task4.js
+++ assets/CSE 121B/Week 4/js/task4.js
@@ -3,7 +3,7 @@
 /* DATA */
 
 // Step 1: Declare a new variable to hold information about yourself
-
+const info = {};
 // Step 2: Inside of the object, add a property named name with a value of your name as a string
 
 // Step 3: Add another property named photo with a value of the image path and name (used in Task 2) as a string
