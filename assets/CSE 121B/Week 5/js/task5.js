14029e2fb220e6b7d5453bee8f25e80c70d7834c
AaronLongstocking97 <jon17049@byui.edu>
2021-12-04 22:20:53 -0700

    Signed-off-by: AaronLongstocking97 <jon17049@byui.edu>

--- assets/CSE 121B/Week 5/js/task5.js
+++ assets/CSE 121B/Week 5/js/task5.js
@@ -3,62 +3,62 @@
 /* IF/ELSE IF */
 
 // Step 1: Declare and initialize a new variable to hold the current date
-const today = new Date();
+const curDate = new Date();
 // Step 2: Declare another variable to hold the day of the week
 let dayOfWeek;
 // Step 3: Using the variable declared in Step 1, assign the value of the variable declared in Step 2 to the day of the week ( hint: getDay() )
-dayOfWeek = today.getDay();
+dayOfWeek = curDate.getDay();
 // Step 4: Declare a variable to hold a message that will be displayed
-let message1;
+let firstMessage;
 // Step 5: Using an if statement, if the day of the week is a weekday (i.e. Monday - Friday), set the message variable to the string 'Hang in there!'
 if (dayOfWeek >= 1 && dayOfWeek <= 5) {
-  message1 = "Hang in there!";
+  firstMessage = "Hang in there!";
 }
 // Step 6: Using an else statement, set the message variable to 'Woohoo!  It is the weekend!'
 if (dayOfWeek >= 1 && dayOfWeek <= 5) {
-  message1 = "Hang in there!";
+  firstMessage = "Hang in there!";
 } else {
-  message1 = "Woohoo!  It is the weekend!";
+  firstMessage = "Woohoo!  It is the weekend!";
 }
 
 /* SWITCH, CASE, BREAK */
 
 // Step 1: Declare a new variable to hold another message
-let message2;
+let secondMessage;
 // Step 2: Use switch, case and break to set the message variable to the day of the week as a string (e.g. Sunday, Monday, etc.) using the day of week variable declared in Step 2 above
 switch (dayOfWeek) {
   case 0:
-    message2 = "Sunday";
+    secondMessage = "Sunday";
     break;
   case 1:
-    message2 = "Monday";
+    secondMessage = "Monday";
     break;
   case 2:
-    message2 = "Tuesday";
+    secondMessage = "Tuesday";
     break;
   case 3:
-    message2 = "Wednesday";
+    secondMessage = "Wednesday";
     break;
   case 4:
-    message2 = "Thursday";
+    secondMessage = "Thursday";
     break;
   case 5:
-    message2 = "Friday";
+    secondMessage = "Friday";
     break;
   case 6:
-    message2 = "Saturday";
+    secondMessage = "Saturday";
     break;
   default:
-    message2 = "Unknown - " + dayOfWeek;
+    secondMessage = "Unknown - " + dayOfWeek;
     break;
 }
 
 /* OUTPUT */
 
 // Step 1: Assign the value of the first message variable to the HTML element with an ID of message1
-document.querySelector("#message1").textContent = message1;
+document.querySelector("#message1").textContent = firstMessage;
 // Step 2: Assign the value of the second message variable to the HTML element with an ID of message2
-document.querySelector("#message2").textContent = message2;
+document.querySelector("#message2").textContent = secondMessage;
 
 /* FETCH */
 
