378866bd391aa69d07ead7ea1c1a1697235d6c81
AaronLongstocking97 <jon17049@byui.edu>
2021-12-09 19:12:43 -0700

    Signed-off-by: AaronLongstocking97 <jon17049@byui.edu>

--- assets/CSE 121B/Week 6/js/task6.js
+++ assets/CSE 121B/Week 6/js/task6.js
@@ -51,20 +51,69 @@ favoriteFoods.pop();
 // Step 7: repeat Step 2
 document.querySelector("#food").innerHTML = FAVFOODS;
 ***/
+function getQuote() {
+  const URL =
+    "https://famous-quotes4.p.rapidapi.com/random?category=all&count=2";
+  fetch(URL, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "famous-quotes4.p.rapidapi.com",
+      "x-rapidapi-key": "22c3f5e831msh69107dfc8e3999dp1d8b43jsn2f0512073699",
+    },
+  })
+    .then(function (response) {
+      console.log(response);
+      if (response.ok) {
+        return response.json();
+      }
+      throw new ERROR("Response not OK.");
+    })
+    .catch((error) => console.log("There was a build error: ", error))
+    .then(function () {
+      // Build the page for viewing
+      buildPage();
+    });
+}
 
+/***
 const url = "https://pokeapi.co/api/v2/pokemon/ditto";
 const results = fetch(url);
 console.log(results);
-fetch("https://famous-quotes4.p.rapidapi.com/random?category=all&count=2", {
-  method: "GET",
-  headers: {
-    "x-rapidapi-host": "famous-quotes4.p.rapidapi.com",
-    "x-rapidapi-key": "22c3f5e831msh69107dfc8e3999dp1d8b43jsn2f0512073699",
-  },
-})
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+
+// Gets current weather information for a specific weather station from the NWS API
+function getWeather(stationId) {
+    // This is the URL for current observation data 
+    const URL = 'https://api.weather.gov/stations/' + stationId + '/observations/latest';
+    // NWS User-Agent header (built above) will be the second parameter 
+    fetch(URL, idHeader)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new ERROR('Response not OK.');
+        })
+        .then(function (data) {
+            // Let's see what we got back
+            console.log('From getWeather function:');
+            console.log(data);
+
+            let temperature = data.properties.temperature.value;
+            let temperatureF = convertToFahrenheit(temperature);
+            let direct = data.properties.windDirection.value;
+            let wDirection = convertWindDirection(direct);
+            console.log("This is the wind direction angle: " + wDirection);
+
+            // Store weather information to localStorage 
+            storage.setItem('temp', temperatureF);
+            storage.setItem('wind', data.properties.windSpeed.value);
+            storage.setItem('direction', wDirection);
+            storage.setItem('summary', data.properties.textDescription);
+
+        })
+        .catch(error => console.log('There was a getWeather error: ', error))
+        .then(function () {
+            // Build the page for viewing 
+            buildPage();
+        })
+} // end getWeather function
+***/
