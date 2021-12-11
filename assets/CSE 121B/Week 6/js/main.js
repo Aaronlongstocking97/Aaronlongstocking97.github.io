659e36a455014af8ed5880d3316d29d58e9df58d
AaronLongstocking97 <jon17049@byui.edu>
2021-12-10 17:13:04 -0700

    Signed-off-by: AaronLongstocking97 <jon17049@byui.edu>

--- assets/CSE 121B/Week 6/js/main.js
+++ assets/CSE 121B/Week 6/js/main.js
@@ -1,7 +1,10 @@
 // toggle menu in small view
 const toggleMenu = () => {
-    document.querySelector('#menu').classList.toggle('open');
-}
+  document.querySelector("#menu").classList.toggle("open");
+};
 
-document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
+document.querySelector("#toggleMenu").addEventListener("click", toggleMenu);
 
+// set current year in footer
+const currentDate = new Date();
+document.querySelector("#year").textContent = currentDate.getFullYear();
--- assets/CSE 121B/Week 6/js/task6.js
+++ assets/CSE 121B/Week 6/js/task6.js
@@ -1,59 +1,7 @@
-/* Project Week 6 */
-/* Random Quote Generator */
-/* VARIABLES */
-
-/*** 
-// Step 1: declare and instantiate a variable to hold your name
-const NAME = "Aaron Jones";
-
-// Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
-document.querySelector("#name").innerHTML = NAME;
-
-// Step 3: declare and instantiate a variable to hold the current year
-const YEAR = 2021;
-
-// Step 4: place the value of the current year variable into the HTML file
-document.querySelector("#year").innerHTML = YEAR;
-
-// Step 5: declare and instantiate a variable to hold the name of your picture
-const PICTURE = "/assets/CSE 121B/Week 2/images/IMG_3803.JPG";
-// Step 6: copy your image into the "images" folder
-
-// Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
-document.querySelector("img").setAttribute("src", PICTURE);
-
- ARRAYS 
-
-// Step 1: declare and instantiate an array variable to hold your favorite foods
-const FAVFOODS = ["Pork", " Sushi", " Ribs", " Bacon"];
-
-// Step 2: place the values of the favorite foods variable into the HTML file
-document.querySelector("#food").innerHTML = FAVFOODS;
-
-// Step 3: declare and instantiate a variable to hold another favorite food
-const ADDITIONALFAVFOOD = " Sausage";
-
-// Step 4: add the variable holding another favorite food to the favorite food array
-FAVFOODS.push(ADDITIONALFAVFOOD);
-
-// Step 5: repeat Step 2
-document.querySelector("#food").innerHTML = FAVFOODS;
-
-// Step 6: remove the first element in the favorite foods array
-FAVFOODS.shift();
-
-// Step 7: repeat Step 2
-document.querySelector("#food").innerHTML = FAVFOODS;
-
-// Step 8: remove the last element in the favorite foods array
-favoriteFoods.pop();
-
-// Step 7: repeat Step 2
-document.querySelector("#food").innerHTML = FAVFOODS;
-***/
-
 /* *************************************
- *  Weather Site JavaScript Functions
+ *  Final Project
+ *  Week 6 - Random Quote Generator
+ *  JavaScript Functions
  ************************************* */
 console.log("My javascript is being read.");
 
@@ -84,6 +32,8 @@ function getQuote() {
       let someText = data[0].text;
 
       console.log("the author is: " + someAuthor);
+      console.log("the category is: " + someCategory);
+      console.log("the text is: " + someText);
 
       // Store data to localstorage
       storage.setItem("someAuthor", someAuthor);
@@ -94,7 +44,7 @@ function getQuote() {
     .then(function () {
       // Build the page for viewing
       buildPage();
-      console.log("buildPage");
+      console.log("buildPage was successful");
     });
 }
 
@@ -112,45 +62,3 @@ function buildPage() {
 var storage = window.localStorage;
 
 getQuote();
-/***
-const url = "https://pokeapi.co/api/v2/pokemon/ditto";
-const results = fetch(url);
-console.log(results);
-
-// Gets current weather information for a specific weather station from the NWS API
-function getWeather(stationId) {
-    // This is the URL for current observation data 
-    const URL = 'https://api.weather.gov/stations/' + stationId + '/observations/latest';
-    // NWS User-Agent header (built above) will be the second parameter 
-    fetch(URL, idHeader)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new ERROR('Response not OK.');
-        })
-        .then(function (data) {
-            // Let's see what we got back
-            console.log('From getWeather function:');
-            console.log(data);
-
-            let temperature = data.properties.temperature.value;
-            let temperatureF = convertToFahrenheit(temperature);
-            let direct = data.properties.windDirection.value;
-            let wDirection = convertWindDirection(direct);
-            console.log("This is the wind direction angle: " + wDirection);
-
-            // Store weather information to localStorage 
-            storage.setItem('temp', temperatureF);
-            storage.setItem('wind', data.properties.windSpeed.value);
-            storage.setItem('direction', wDirection);
-            storage.setItem('summary', data.properties.textDescription);
-
-        })
-        .catch(error => console.log('There was a getWeather error: ', error))
-        .then(function () {
-            // Build the page for viewing 
-            buildPage();
-        })
-} // end getWeather function
-***/
