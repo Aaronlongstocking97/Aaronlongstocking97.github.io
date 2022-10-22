c3aa7cbc2993b7d6a11bb9bafccb5f677fda8024
Aaron  Jones <jon17049@byui.edu>
2022-10-21 20:59:45 -0600

    Signed-off-by: Aaron  Jones <jon17049@byui.edu>
    changes made

--- phpmotors/common/footer.php
+++
@@ -1,5 +0,0 @@
-<p>&copy; PHP Motors, All rights reserved.</p>
-<p>All images used are believed to be in "Fair Use".
-    Please notify the author if any are not and they
-    will be removed.</P>
-<p id="update">Last Updated: 8 May, 2021</p>
